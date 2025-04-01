e8a805db846f91ebad49f9f34bc6817d5ec981fe
ppenkof <overton@abv.bg>
2025-04-01 08:28:17 +0300

    login and register are ready

--- JS_Application/App_Examp_Prepare/ExapPrep9April2024/scr/views/login.js
+++ JS_Application/App_Examp_Prepare/ExapPrep9April2024/scr/views/login.js
@@ -9,24 +9,28 @@ export default async function tattooLoginPage() {
 }
 
 function loginTemplate() {
-  return html`<section id="login">
-    <div class="form">
-      <h2>Login</h2>
-      <form @submit=${loginUser} class="login-form">
-        <input type="text" name="email" id="email" placeholder="email" />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="password"
-        />
-        <button type="submit">login</button>
-        <p class="message">
-          Not registered? <a href="/register">Create an account</a>
-        </p>
-      </form>
-    </div>
-  </section>`;
+  return html`
+    <!-- Login Page (Only for Guest users) -->
+    <section id="login">
+      <div class="form">
+        <img class="border" src="./images/border.png" alt="" />
+        <h2>Login</h2>
+        <form @submit=${loginUser} class="login-form">
+          <input type="text" name="email" id="email" placeholder="email" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+          />
+          <button type="submit">login</button>
+          <p class="message">
+            Not registered? <a href="/register">Create an account</a>
+          </p>
+        </form>
+      </div>
+    </section>
+  `;
 }
 
 async function loginUser(e) {
