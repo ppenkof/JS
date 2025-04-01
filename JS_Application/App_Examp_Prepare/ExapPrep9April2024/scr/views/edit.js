e2cfcd604084046cd53a99686b5c02cb9d5f915a
ppenkof <overton@abv.bg>
2025-04-01 09:08:38 +0300

    updated eedit.js

--- JS_Application/App_Examp_Prepare/ExapPrep9April2024/scr/views/edit.js
+++ JS_Application/App_Examp_Prepare/ExapPrep9April2024/scr/views/edit.js
@@ -4,71 +4,70 @@ import itemsService from "../api/itemsService.js";
 
 const mainEl = document.querySelector("main");
 
-export default async function tattooEditPage(ctx) {
-  const tattooId = ctx.params.id;
-  const tattoo = await itemsService.getById(tattooId);
-  render(editTemplate(tattoo), mainEl);
+export default async function solutionEditPage(ctx) {
+  const solutionId = ctx.params.id;
+  const solution = await itemsService.getById(solutionId);
+  render(editTemplate(solution), mainEl);
 }
 
-function editTemplate(tattoo) {
-  return html`<section id="edit">
-    <div class="form">
-      <h2>Edit tattoo</h2>
-      <form @submit=${(e) => editTattoo(e, tattoo._id)} class="edit-form">
-        <input
-          type="text"
-          name="type"
-          id="type"
-          placeholder="Tattoo Type"
-          value=${tattoo.type}
-        />
-        <input
-          type="text"
-          name="image-url"
-          id="image-url"
-          placeholder="Image URL"
-          value=${tattoo.imageUrl}
-        />
-        <textarea
-          id="description"
-          name="description"
-          placeholder="Description"
-          rows="2"
-          cols="10"
-          .value=${tattoo.description}
-        ></textarea>
-        <select
-          id="user-type"
-          name="user-type"
-          .value=${tattoo["user-type"] || tattoo.userType}
-        >
-          <option value="" disabled selected>Select your role</option>
-          <!---->
-          <option value="Tattoo Artist">Tattoo Artist</option>
-          <option value="Tattoo Enthusiast">Tattoo Enthusiast</option>
-          <option value="First Time in Tattoo">First Time in Tattoo</option>
-          <option value="Tattoo Collector">Tattoo Collector</option>
-        </select>
-        <button type="submit">Edit</button>
-      </form>
-    </div>
-  </section>`;
+function editTemplate(solution) {
+  return html`
+    <!-- Edit Page (Only for logged-in users) -->
+    <section id="edit">
+      <div class="form">
+        <img class="border" src="./images/border.png" alt="" />
+        <h2>Edit Solution</h2>
+        <form @submit=${(e) => editSolution(e, solution._id)} class="edit-form">
+          <input
+            type="text"
+            name="type"
+            id="type"
+            placeholder="Solution Type"
+            value=${solution.type}
+          />
+          <input
+            type="text"
+            name="image-url"
+            id="image-url"
+            placeholder="Image URL"
+            value=${solution.imageUrl}
+          />
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            rows="2"
+            cols="10"
+            .value=${solution.description}
+          ></textarea>
+          <textarea
+            id="more-info"
+            name="more-info"
+            placeholder="more Info"
+            rows="2"
+            cols="10"
+            .value=${solution.moreInfo}
+          ></textarea>
+          <button type="submit">Edit</button>
+        </form>
+      </div>
+    </section>`;
 }
 
-async function editTattoo(e, tattooId) {
+async function editSolution(e, solutionId) {
   e.preventDefault();
 
   const formData = new FormData(e.currentTarget);
-  const tattooData = Object.fromEntries(formData);
-  console.log(tattooData);
-  if (Object.values(tattooData).some((val) => val === "")) {
+  const solutionData = Object.fromEntries(formData);
+  console.log(solutionData);
+  if (Object.values(solutionData).some((val) => val === "")) {
     return alert("All fields are required!");
   }
 
   try {
-    const result = await itemsService.update(tattooId, tattooData);
+    const result = await itemsService.update(solutionId, solutionData);
 
-    page.redirect(`/details/${tattooId}`);
+    page.redirect(`/details/${solutionId}`);
   } catch (err) {
     alert(err.message);
   }
