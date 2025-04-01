2f627dc17fb9d105072843550240dd9c570cc748
ppenkof <overton@abv.bg>
2025-04-01 09:01:39 +0300

    updated create.js

--- JS_Application/App_Examp_Prepare/ExapPrep9April2024/scr/app.js
+++ JS_Application/App_Examp_Prepare/ExapPrep9April2024/scr/app.js
@@ -1,24 +1,24 @@
 import page from '../node_modules/page/page.mjs';
 
-import tattooCreatePage from './views/create.js';
-import tattooDashboardPage from './views/dashboard.js';
-import tattooDetailsPage from './views/details.js';
-import tattooEditPage from './views/edit.js';
-import tattooHomePage from './views/home.js';
-import tattooLoginPage from './views/login.js';
-import tattooRegisterPage from './views/register.js';
-//import tattooSearchPage from './views/search.js';
-import tattooNavigation from './middlewares/navigation.js';
+import solutionCreatePage from './views/create.js';
+import solutionDashboardPage from './views/dashboard.js';
+import solutionDetailsPage from './views/details.js';
+import solutionEditPage from './views/edit.js';
+import solutionHomePage from './views/home.js';
+import solutionLoginPage from './views/login.js';
+import solutionRegisterPage from './views/register.js';
+//import solutionSearchPage from './views/search.js';
+import solutionNavigation from './middlewares/navigation.js';
 
-page(tattooNavigation);
-page('/', tattooHomePage);
-page('/register', tattooRegisterPage);
-page('/login', tattooLoginPage);
+page(solutionNavigation);
+page('/', solutionHomePage);
+page('/register', solutionRegisterPage);
+page('/login', solutionLoginPage);
 
-page('/dashboard', tattooDashboardPage);
-page('/create', tattooCreatePage);
-page('/details/:id', tattooDetailsPage);
-page('/edit/:id', tattooEditPage);
-//page('/search', tattooSearchPage);
+page('/dashboard', solutionDashboardPage);
+page('/create', solutionCreatePage);
+page('/details/:id', solutionDetailsPage);
+page('/edit/:id', solutionEditPage);
+//page('/search', solutionSearchPage);
 
 page.start();
--- JS_Application/App_Examp_Prepare/ExapPrep9April2024/scr/views/create.js
+++ JS_Application/App_Examp_Prepare/ExapPrep9April2024/scr/views/create.js
@@ -5,57 +5,64 @@ import itemsService from "../api/itemsService.js";
 
 const mainEl = document.querySelector("main");
 
-export default async function tattooCreatePage() {
-  //const tattooId = ctx.params.id; //here you can decide if user is logged!!!
+export default async function solutionCreatePage() {
+  //const solutionId = ctx.params.id; //here you can decide if user is logged!!!
   render(createTemplate(), mainEl);
 }
 
 function createTemplate() {
-  return html`<section id="create">
-    <div class="form">
-      <h2>Add tattoo</h2>
-      <form @submit=${addTattoo} class="create-form">
-        <input type="text" name="type" id="type" placeholder="Tattoo Type" />
-        <input
-          type="text"
-          name="image-url"
-          id="image-url"
-          placeholder="Image URL"
-        />
-        <textarea
-          id="description"
-          name="description"
-          placeholder="Description"
-          rows="2"
-          cols="10"
-        ></textarea>
-        <!--It have to be checked if have restriction for logged users-->
-        <select id="user-type" name="user-type">
-          <option>Select your role</option>
-          <!--value="" disabled selected-->
-          <option value="Tattoo Artist">Tattoo Artist</option>
-          <option value="Tattoo Enthusiast">Tattoo Enthusiast</option>
-          <option value="First Time in Tattoo">First Time in Tattoo</option>
-          <option value="Tattoo Collector">Tattoo Collector</option>
-        </select>
-        <button type="submit">Add tattoo</button>
-      </form>
-    </div>
-  </section>`;
+  return html`
+    <!-- Create Page (Only for logged-in users) -->
+    <section id="create">
+      <div class="form">
+        <img class="border" src="./images/border.png" alt="" />
+        <h2>Add Solution</h2>
+        <form @submit=${addSolution} class="create-form">
+          <input
+            type="text"
+            name="type"
+            id="type"
+            placeholder="Solution Type"
+          />
+          <input
+            type="text"
+            name="image-url"
+            id="image-url"
+            placeholder="Image URL"
+          />
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            rows="2"
+            cols="10"
+          ></textarea>
+          <textarea
+            id="more-info"
+            name="more-info"
+            placeholder="more Info"
+            rows="2"
+            cols="10"
+          ></textarea>
+          <button type="submit">Add Solution</button>
+        </form>
+      </div>
+    </section>
+  `;
 }
 
-async function addTattoo(e) {
+async function addSolution(e) {
   e.preventDefault();
 
   const formData = new FormData(e.currentTarget);
-  const tattooData = Object.fromEntries(formData);
-  console.log(tattooData);
-  if (Object.values(tattooData).some((val) => val === "")) {
+  const solutionData = Object.fromEntries(formData);
+  console.log(solutionData);
+  if (Object.values(solutionData).some((val) => val === "")) {
     return alert("All fields are required!");
   }
 
   try {
-    const result = await itemsService.create(tattooData);
+    const result = await itemsService.create(solutionData);
     page.redirect("/dashboard");
   } catch (err) {
     alert(err.message);
