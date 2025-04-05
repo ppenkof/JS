16b7718c3b9c42f9eabe36a9a75b764f851b50f3
ppenkof <overton@abv.bg>
2025-04-05 15:12:35 +0300

    dashboard updated

--- JS_Application/App_Examp_Prepare/ExamRegular/Vintage_Stamps_Resources/scr/views/dashboard.js
+++ JS_Application/App_Examp_Prepare/ExamRegular/Vintage_Stamps_Resources/scr/views/dashboard.js
@@ -3,37 +3,39 @@ import itemsService from "../api/itemsService.js";
 
 const mainEl = document.querySelector("main");
 
-export default async function tattooDashboardPage() {
+export default async function stampDashboardPage() {
   const tattoos = await itemsService.getAll();
   //console.log(tattoos); // it have to check totoo.objAtributes
   render(dashboardTemplate(tattoos), mainEl);
 }
 
-function dashboardTemplate(tattoos) {
+function dashboardTemplate(stamps) {
   return html`
+    <!-- Dashboard page -->
     <h2>Collection</h2>
-
-    ${tattoos.length > 0
+    ${stamps.length > 0
       ? html`
-          <section id="tattoos">
-            ${tattoos.map((s) => tattooTemplate(s))}
+          <section id="collection">
+            ${stampss.map((s) => stampTemplate(s))}
           </section>
         `
-      : html` <h2 id="no-tattoo">
-          Collection is empty, be the first to contribute
-        </h2>`}
+      : html`<!-- Display an h2 if there are no posts -->
+          <h2 id="no-stamp">No Stamps Added.</h2>`}
   `;
 }
 // it have to be checked if totoo.objAtributes is correct
-function tattooTemplate(tattoo) {
-
-  return html`<div class="tattoo">
-    <img src=${tattoo.imageUrl} alt="example1" />
-    <div class="tattoo-info">
-      <h3 class="type">${tattoo.type}</h3>
-      <span>Uploaded by </span>
-      <p class="user-type">${tattoo.userType || tattoo["user-type"]}</p>
-      <a class="details-btn" href="/details/${tattoo._id}">Learn More</a>
+function stampTemplate(stamp) {
+  return html`
+    <!-- Display a div with information about every post (if any)-->
+    <div class="stamp">
+      <img src=${stamp.imageUrl} alt="example1" />
+      <div class="stamp-info">
+        <h3 class="name">${stamp.name}</h3>
+        <p class="year-description">
+          Year of oldest stamps - <span class="year">${stamp.year}</span>
+        </p>
+        <a class="learn-more-btn" href="/details/${stamp._id}">Learn More</a>
+      </div>
     </div>
-  </div>`;
+  `;
 }
