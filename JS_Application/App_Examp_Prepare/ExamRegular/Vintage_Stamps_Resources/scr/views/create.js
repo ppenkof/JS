d9e769b76b511287a69d4c461695ab4ca135b2ca
ppenkof <overton@abv.bg>
2025-04-05 15:16:13 +0300

    updated create

--- JS_Application/App_Examp_Prepare/ExamRegular/Vintage_Stamps_Resources/scr/views/create.js
+++ JS_Application/App_Examp_Prepare/ExamRegular/Vintage_Stamps_Resources/scr/views/create.js
@@ -5,57 +5,62 @@ import itemsService from "../api/itemsService.js";
 
 const mainEl = document.querySelector("main");
 
-export default async function tattooCreatePage() {
-  //const tattooId = ctx.params.id; //here you can decide if user is logged!!!
+export default async function stampCreatePage() {
+  //const stampId = ctx.params.id; //here you can decide if user is logged!!!
   render(createTemplate(), mainEl);
 }
 
 function createTemplate() {
-  return html`<section id="create">
-    <div class="form">
-      <h2>Add tattoo</h2>
-      <form @submit=${addTattoo} class="create-form">
-        <input type="text" name="type" id="type" placeholder="Tattoo Type" />
-        <input
-          type="text"
-          name="image-url"
-          id="image-url"
-          placeholder="Image URL"
-        />
-        <textarea
-          id="description"
-          name="description"
-          placeholder="Description"
-          rows="2"
-          cols="10"
-        ></textarea>
-        <!--It have to be checked if have restriction for logged users-->
-        <select id="user-type" name="user-type">
-          <option>Select your role</option>
-          <!--value="" disabled selected-->
-          <option value="Tattoo Artist">Tattoo Artist</option>
-          <option value="Tattoo Enthusiast">Tattoo Enthusiast</option>
-          <option value="First Time in Tattoo">First Time in Tattoo</option>
-          <option value="Tattoo Collector">Tattoo Collector</option>
-        </select>
-        <button type="submit">Add tattoo</button>
-      </form>
-    </div>
-  </section>`;
+  return html`
+    <!-- Create Page (Only for logged-in users) -->
+    <section id="create">
+      <div class="form">
+        <h2>Add Post Stamp</h2>
+        <form @submit=${addStamp} class="create-form">
+          <input
+            type="text"
+            name="name-input"
+            id="name-input"
+            placeholder="Stamp Name"
+          />
+          <input
+            type="text"
+            name="image-url-input"
+            id="image-url-input"
+            placeholder="Image URL"
+          />
+          <input
+            type="number"
+            id="year-input"
+            name="year-input"
+            placeholder="year"
+          />
+          <textarea
+            id="more-info-textarea"
+            name="more-info-textarea"
+            placeholder="More Info"
+            rows="8"
+            cols="10"
+          ></textarea>
+          <button type="submit">Add Stamp</button>
+        </form>
+      </div>
+    </section>
+  `;
 }
 
-async function addTattoo(e) {
+async function addStamp(e) {
   e.preventDefault();
 
   const formData = new FormData(e.currentTarget);
-  const tattooData = Object.fromEntries(formData);
-  console.log(tattooData);
-  if (Object.values(tattooData).some((val) => val === "")) {
+  const stampData = Object.fromEntries(formData);
+  console.log(stampData);
+  if (Object.values(stampData).some((val) => val === "")) {
     return alert("All fields are required!");
   }
 
   try {
-    const result = await itemsService.create(tattooData);
+    const result = await itemsService.create(stampData);
     page.redirect("/dashboard");
   } catch (err) {
     alert(err.message);
