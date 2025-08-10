c08470902153c563a874f627b1ae3293907ac49a
ppenkof <overton@abv.bg>
2025-08-10 13:08:14 +0300

    Create Home component

--- Angular/WorkshopComponents/WorkshopPart/TestApp/MyApp/src/app/features/home/home.ts
+++ Angular/WorkshopComponents/WorkshopPart/TestApp/MyApp/src/app/features/home/home.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
+private authService = inject(AuthService);
+readonly isLoggedIn = this.authService.isLoggedIn;
+readonly currentUser = this.authService.currentUser;
 }
