97452bf1a4bf724a0fbe12b5d5733254a2f57a78
ppenkof <overton@abv.bg>
2025-10-23 14:14:54 +0300

    Render static assets in homepage

--- JS_Back_End/07-ExamPrep_skeleton/src/controllers/homeController.js
+++ JS_Back_End/07-ExamPrep_skeleton/src/controllers/homeController.js
@@ -1,13 +1,11 @@
 import { Router } from "express";
 
 const homeController = Router();
+
 homeController.get('/', (req, res) => {
-    res.send('Welcome to the Home Page!');
+    res.render('home', {layout: false});
 });
 
-homeController.post('/', (req, res) => {
-    const formData = req.body;
-    res.send(`Form submitted `);
-});
+
 
 export default homeController;
