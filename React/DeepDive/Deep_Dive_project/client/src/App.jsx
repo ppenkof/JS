bb65f3d24efafdefbb6520f0f230777e7a52b2c6
ppenkof <overton@abv.bg>
2025-11-09 22:33:29 +0200

    Add React base components

--- React/DeepDive/Deep_Dive_project/client/src/App.jsx
+++ React/DeepDive/Deep_Dive_project/client/src/App.jsx
@@ -1,9 +1,28 @@
-
+import Header from "./components/Header.jsx"
+import Footer from "./components/Footer.jsx"
+import Search from "./components/Search.jsx"
+import Pagination from "./components/Pagination.jsx"
+import UserList from "./components/UserList.jsx"
 
 function App() {
   return (
    <div>
-      <h1>Hello</h1>
+      <Header/>
+
+    <main className="main">
+      <section className="card users-container">
+        
+        <Search/>
+
+        <UserList/>
+
+
+        <Pagination/>
+       
+      </section>
+    </main>
+
+      <Footer/>
    </div>
   )
 }
---
+++ React/DeepDive/Deep_Dive_project/client/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (
+        <footer className="footer">
+            <p>This site is designed to be used htmlFor training purposes at softUni.</p>
+        </footer>
+    );
+}
